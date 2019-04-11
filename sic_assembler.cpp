af8ce276dd681d22f5c91acf0ff0e18877162040
Hagar Usama <Hagar_usama@outlook.com>
2019-04-11 23:44:06 +0200

    split added

--- sic_assembler.cpp
+++ sic_assembler.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <map>
+#include <string>
 //#include <bits/stsc++.h>
 using namespace std;
 
@@ -12,27 +13,60 @@ using namespace std;
 //std::multiset<T>
 //std::multimap<T>
 
+string sub_line(string line ,int length);
+string split(string &line ,int length);
+void edit(string& s) {
+    s[1] = 'A';
+}
+
+
 int main(){
 
-map<string , float> s;
+//////////////////// mapping ////////////////
+map<string , float> ss;
 
-string x = "djjg";
+ss["a"] = 2000;
+ss["dd"] = 3000;
+ss["ed"] = 4000;
 
-cout<<x<<endl;
+///////////////////////////////////////////////
 
-s["a"] = 2000;
-s["dd"] = 3000;
-s["ed"] = 4000;
-
-cout<<s["a"]<<endl;
-
+/////////////////// mapping and pair //////////
 pair <string ,int> xz;
 xz.first = "aaa";
 xz.second = 5;
+ss.insert(xz);
+cout<<ss["aaa"]<<endl;
+////////////////////////////////////////////////
 
+string str("0123456789asdf");
+string str2("ss");
+str2 = split(str, 5);
+cout<<str2<<endl;
+cout<<str<<endl;
 
-s.insert(xz);
-cout<<s["aaa"]<<endl;
+/*
+for (unsigned i = 0; i < str.length(); i += 4) {
+    cout << str.substr(i, 4) << endl;
+}
+*/
+
+/*
+
+std::string s = "scott>=tiger";
+    std::string delim = ">=";
+
+    auto start = 0U;
+    auto end = s.find(delim);
+    while (end != std::string::npos)
+    {
+        std::cout << s.substr(start, end - start) << std::endl;
+        start = end + delim.length();
+        end = s.find(delim, start);
+    }
+
+    std::cout << s.substr(start, end);
+*/
 
 /*
 queue<int> myQ;
@@ -53,4 +87,23 @@ for(int i=0 ; i<10;i++){
 	}
 */
 return 0;
+}
+
+string sub_line(string line ,int length){
+	
+	cout <<line.substr(0,length)<<endl;
+	
+	line = line.substr(length , line.capacity());
+	return line;	
+}
+	
+	
+	
+string split(string &line ,int length){
+	
+	string token = line.substr(0,length);
+	line = line.substr(length , line.capacity());
+	cout <<line.substr(0,length)<<endl;
+	
+	return token;	
 }

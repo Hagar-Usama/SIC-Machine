26631538b7171cac0b3e2d436a6d4a6e68f9e2e9
Hagar Usama <Hagar_usama@outlook.com>
2019-04-20 14:26:33 +0200

    working on partitioning directive statement .. trim done

--- regex_val.cpp
+++ regex_val.cpp
@@ -8,7 +8,7 @@
  	 * 		then the next part is now the first one, we extract it as if it was the first
  	 * 		regular expression suggested is "^[ |\t]*\\b('exact expression we search for without quotation mark')([^ ]*)"
  	 * */
- 	
+// >>trim>> http://www.martinbroadhurst.com/how-to-trim-a-stdstring.html
 #include <iostream>
 #include <regex>
 #include <string>
@@ -17,19 +17,34 @@
  
  int check_dir_lab( string exp);
  int check_dir_unlab( string exp);
+ void partition_dir(bool label , string exp);
+ int get_matched(string s , regex reg , string &mat);
  
+std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r "); 
+std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ");
+std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ");
+
 int main(){
+	bool labeled;
 	
 	string input;
  	while(true)
  	{
-
- 		//cout<<"labeled mode\n";
- 		//getline (cin, input);
- 		//check_dir_lab( input);
- 		cout<<"unlabeled mode\n";
+ 		cout<<"labeled mode\n";
  		getline (cin, input);
- 		check_dir_unlab( input); 		
+ 		if(check_dir_lab( input)){
+			labeled = true;
+			string reg = "^(\\s*(\\b(){1}\\w{1,7})\\s+)";
+			regex re (reg);
+			string pmatch;
+			get_matched(input, re , pmatch);
+			pmatch = trim(pmatch);
+			cout<<endl<<"**"<<pmatch<<"**\n";
+			//partition_dir(labeled , input);
+		}else if(check_dir_unlab( input)){
+			labeled = false;
+			}
+			
  		 	}
 
 return 0;
@@ -91,7 +106,7 @@ int check_dir_unlab( string exp){
 	 //(\\b(base)\\s+((\\b(){1}\\w{1,7})|\\*))
 	 //(\\b(org)(\\s+\\b(){1}\\w{1,7}))
 	 string input;
-	 string reg = "\\s*((\\b(start)\\s+[a-f0-9]{1,4})|(\\b(byte)\\s+((c'.{1,14}')|(x'[a-f0-9]{1,15}')))|(\\b(word)\\s+-?\\d{1,4}(,-?\\d{1,4})*)|(\\b(resw|resb)\\s+\\d{1,4})|(\\b(equ)\\s+\\b([a-z]){1}\\w{1,7}(\\+\\d{1,4})?))\\s*(;.*)?";
+	 string reg = "\\s*((\\b(start)\\s+[a-f0-9]{1,4})|(\\b(byte)\\s+((c'.{1,14}')|(x'[a-f0-9]{1,15}')))|(\\b(word)\\s+-?\\d{1,4}(,-?\\d{1,4})*)|(\\b(resw|resb)\\s+\\d{1,4})|(\\b(end)(\\s+\\b(){1}\\w{1,7})?)|(\\b(org)(\\s+\\b(){1}\\w{1,7}))|(\\b(base)\\s+((\\b(){1}\\w{1,7})|\\*)))\\s*(;.*)?";
 	 
 	 regex re(reg);
 		
@@ -104,4 +119,51 @@ int check_dir_unlab( string exp){
  			cout<<"Invalid input"<<endl;
  			return 0;
  		}
+}
+
+
+int get_matched(string s , regex reg, string &mat){
+	
+	smatch match;
+	if(regex_search(s,match,reg) == true){
+		cout<<"Match Size = "<<match.size()<<endl;
+		cout<<match.str(0)<<endl;
+		cout<<match.str(1)<<endl;
+		mat = match.str(1);
+		return 1;
+		}
+	return 0;
+	
+	}
+void partition_dir(bool label , string exp){
+	
+
+	if(label){
+		    
+    regex reg("[^\\w]+");
+    exp = regex_replace(exp, reg, " ");
+    cout << exp << endl;
+    
+		
+		}else{}
+	
+	
+	}
+
+
+ std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+    str.erase(0, str.find_first_not_of(chars));
+    return str;
+}
+ 
+std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+    str.erase(str.find_last_not_of(chars) + 1);
+    return str;
+}
+ 
+std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+{
+    return ltrim(rtrim(str, chars), chars);
 }

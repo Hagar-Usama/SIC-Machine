d74f26511adf6984ec40c61cd52ec3313a10e537
Hagar Usama <Hagar_usama@outlook.com>
2019-04-20 02:36:43 +0200

    validation functions here

---
+++ regex_val.cpp
@@ -0,0 +1,74 @@
+/***
+ 	 * My algorithm for validating SIC instruction using RegEx
+ 	 * 
+ 	 * 
+ 	 * Part II (extracting data form accepted expression)
+ 	 * 		assume we know : (statement/directive) , (which format/type) , (labeled or not)
+ 	 * 		we extract part by part, then replace extracted part by a whitespace
+ 	 * 		then the next part is now the first one, we extract it as if it was the first
+ 	 * 		regular expression suggested is "^[ |\t]*\\b('exact expression we search for without quotation mark')([^ ]*)"
+ 	 * */
+ 	
+#include <iostream>
+#include <regex>
+#include <string>
+
+ using namespace std;
+ 
+ int check_dir_lab( string exp);
+
+int main(){
+	
+	string input;
+ 	while(true)
+ 	{
+ 		cout<<"Enter an expression!"<<endl;
+ 		getline (cin, input);
+ 		check_dir_lab( input); 		
+ 		 	}
+
+return 0;
+
+	}
+
+ int check_dir_lab( string exp){
+	 
+	 string input;
+	 string reg = "\\s*(\\b([a-z]){1}\\w{1,7})\\s+((\\b(start)\\s+[a-f0-9]{1,4})|(\\b(byte)\\s+((c'.{1,14}')|(x'[a-f0-9]{1,15}')))|(\\b(word)\\s+-?\\d{1,4}(,-?\\d{1,4})*)|(\\b(resw|resb)\\s+\\d{1,4})|(\\b(equ)\\s+\\b([a-z]){1}\\w{1,7}(\\+\\d{1,4})?))\\s*(;.*)?";
+	 
+	 regex re(reg);
+		
+ 		if(regex_match(exp,re)){
+			cout<<"Valid"<<endl;
+			return 1;		
+			}
+ 		else
+ 		{
+ 			cout<<"Invalid input"<<endl;
+ 			return 0;
+ 		}
+
+		
+/*
+label :
+		\\s*(\\b(){1}\\w{1,7})\\s+
+START :
+		(\\b(start)\\s+\\[a-f0-9]{4})
+BYTE :
+		(\\b(byte)\\s+((c'.{1,14}')|(x'[a-f0-9]{1,15}')))
+WORD :
+		(\\b(word)\\s+-?\\d{1,4}(,-?\\d{1,4})*)
+
+RESEB / RESW
+		(\\b(resw|resb)\\s+\\d{1,4})
+
+EQU :
+		(\\b(equ)\\s+\\b(){1}\\w{1,7}(+\\d{1,4})?)
+comment:
+		(;.*)?
+
+
+ */
+
+
+	 }

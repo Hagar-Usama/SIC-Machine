7294ee340de61cef469e61a062e0d7ae4b2d1f77
Hagar Usama <Hagar_usama@outlook.com>
2019-04-21 14:23:16 +0200

    format edit addr --> 7-->2

--- Ophash.cpp
+++ Ophash.cpp
@@ -16,16 +16,29 @@ class Ophash{
 Ophash::Ophash(){}
 
 int main(){
-vector< pair <int,int> > operation;
-vector<string> statement;
+vector< pair <int,int> > operation; // #operands , formatSum 2,3,4
+vector<string> statement;  //operation or directive
+
 operation.push_back(make_pair(2,2)); //RMO 
-operation.push_back(make_pair(1,7)); //LDR
-operation.push_back(make_pair(1,7)); //STR
+operation.push_back(make_pair(1,7)); //LDA
+operation.push_back(make_pair(1,7)); //LDB
+operation.push_back(make_pair(1,7)); //LDF
+operation.push_back(make_pair(1,7)); //LDL
+operation.push_back(make_pair(1,7)); //LDS
+operation.push_back(make_pair(1,7)); //LDT
+operation.push_back(make_pair(1,7)); //LDX
+operation.push_back(make_pair(1,7)); //STA
+operation.push_back(make_pair(1,7)); //STB
+operation.push_back(make_pair(1,7)); //STF
+operation.push_back(make_pair(1,7)); //STL
+operation.push_back(make_pair(1,7)); //STS
+operation.push_back(make_pair(1,7)); //STT
+operation.push_back(make_pair(1,7)); //STX
 operation.push_back(make_pair(1,7)); //LDCH
 operation.push_back(make_pair(1,7)); //STCH
 operation.push_back(make_pair(1,7)); //ADD
 operation.push_back(make_pair(1,7)); //SUB
-operation.push_back(make_pair(2,7)); //ADDR
+operation.push_back(make_pair(2,2)); //ADDR
 operation.push_back(make_pair(2,2)); //SUBR
 operation.push_back(make_pair(2,7)); //COMP
 operation.push_back(make_pair(2,2)); //COMPR
@@ -36,21 +49,31 @@ operation.push_back(make_pair(1,7)); //JGT
 operation.push_back(make_pair(1,7)); //TIX
 operation.push_back(make_pair(1,2)); //TIXR
 
-
-
 operation.push_back(make_pair(0,2)); //start
 operation.push_back(make_pair(0,0)); //end
-operation.push_back(make_pair(0,2)); //byte*
-operation.push_back(make_pair(0,2)); //word*
-operation.push_back(make_pair(0,2)); //resw*
-operation.push_back(make_pair(0,2)); //rsub*
+operation.push_back(make_pair(0,2)); //byte
+operation.push_back(make_pair(0,2)); //word
+operation.push_back(make_pair(0,2)); //resw
+operation.push_back(make_pair(0,2)); //resb
 operation.push_back(make_pair(0,1)); //equ
 operation.push_back(make_pair(0,0)); //org
 operation.push_back(make_pair(0,0)); //base
 
 statement.push_back("rmo");
-statement.push_back("ldr");
-statement.push_back("str");
+statement.push_back("lda");
+statement.push_back("ldb");
+statement.push_back("ldf");
+statement.push_back("ldl");
+statement.push_back("lds");
+statement.push_back("ldt");
+statement.push_back("ldx");
+statement.push_back("sta");
+statement.push_back("stb");
+statement.push_back("stf");
+statement.push_back("stl");
+statement.push_back("sts");
+statement.push_back("stt");
+statement.push_back("stx");
 statement.push_back("ldch");
 statement.push_back("stch");
 statement.push_back("add");
@@ -78,28 +101,27 @@ statement.push_back("equ");
 statement.push_back("org");
 statement.push_back("base");
 
+
 map <string ,pair<int,int>> st_map;
 for (int i=0 ; i<26 ; i++){
 		st_map[statement[i]] = operation[i];
 	}
 
+
+cout<<st_map["start"].first<<endl;
+
+/*
 map<string , pair<int,int>>::iterator p;
 string s="star1t";
+
+pair<int,int> temp;
+
 p = st_map.find(s);
 if(p!= st_map.end()){
+
 	cout<<p->second.second<<endl;
 	}else{
 		cout<<"Not found"<<endl;
 		}
-
-	
-map<string , float> ss;
-
-ss["a"] = 2000;
-ss["dd"] = 3000;
-ss["ed"] = 4000;
-	
-	
-return 0;
-
-	}
+*/
+return 0;}

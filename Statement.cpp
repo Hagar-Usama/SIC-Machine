c3cab9870cc60269b606e63d2e109a440ef87691
Hagar Usama <Hagar_usama@outlook.com>
2019-04-27 22:03:09 +0200

    trim_ in its way to format4

--- Statement.cpp
+++ Statement.cpp
@@ -15,55 +15,47 @@ class Statement{
 	 string label;
 	 string operation;
 	 string operand;
-	 string operand1;
+	 //string operand1;
 	 string operand2;
 	 int indexed;
 	 string comment;
 	 int storage;
 	 int store_op;
-	 bool right;
+	 bool labeled;
 	 int formattype;
-	
+	 int error;
 	Statement(){};
 	 
  int check_indexed();
- //line
- int check_statment();	 
- //line
+ int check_statement();	 
  int check_comment();
- //line
  int check_format4_lab();
- //line
  int check_format4_unlab();	 
- //line
  int check_format3_lab();
- //line
  int check_format3_unlab();	 
- //line
  int check_format2_lab();
- //line
  int check_format2_unlab();
- //line
  int check_dir_lab();
- //line
  int check_dir_unlab();
  
  void divide_operands();
- //line
- void partition_dir(bool label);
- //line
- //void parition_all(bool label , string exp );
- //line
+
+
+ void partition_dir();
+
  void get_partitioned();
- //line
- void partition_format4(bool label);
+
+ void partition_format4();
  
  
  int get_matched(string s , regex reg , string &mat);
+ //void trim(string &str , string delim = "");
  void trim(string &str);
  string extract(string &exp , string re);
  void ltrim(string &exp);
  void rtrim(string &exp);
+ void trim_(string &s , string delim="");
+ 
 	};
 
 
@@ -83,8 +75,9 @@ int Statement:: check_indexed(){
 		 }
 		 
 		 
-int Statement::check_statment(){
+int Statement::check_statement(){
 		 /**
+		  * modified : now label and format included in each check
 	 * return -1 if error
 	 * return  0 if comment
 	 * return 10 if dir 	 unlabeled
@@ -97,23 +90,26 @@ int Statement::check_statment(){
 	 * return 41 if format 4 labeled
 	 * 
 	 * */
-	if(check_comment()) return 0;
-	if(check_dir_unlab())return 10;
-	if(check_dir_lab())return 11;
-	if(check_format2_unlab())return 20;
-	if(check_format2_lab())return 21;
-	if(check_format3_unlab())return 30;
-	if(check_format3_lab())return 31;
-	if(check_format4_unlab())return 40;
-	if(check_format4_lab())return 41;
+	if(check_comment()) return 1;
+	if(check_dir_unlab())return 1;
+	if(check_dir_lab())return 1;
+	if(check_format2_unlab())return 1;
+	if(check_format2_lab())return 1;
+	if(check_format3_unlab())return 1;
+	if(check_format3_lab())return 1;
+	if(check_format4_unlab())return 1;
+	if(check_format4_lab())return 1;
+	
 	return -1;	 
 		 }
 		 	 
 	 
 int Statement::check_comment(){
-		 string reg = "\\s*\\..*"; 
+	string reg = "\\s*\\..*"; 
 	 regex re(reg);
  		if(regex_match(this->line,re)){
+			this->labeled = false;
+			this->formattype = 0;
 			return 1;		
 			}
  	
@@ -126,7 +122,8 @@ int Statement::check_format4_lab(){
 		string reg = "\\s*((\\b([a-z]){1}\\w{0,7})\\s+)\\+\\b(lda|ldb|ldf|ldl|lds|ldt|ldx|sta|stb|stf|stl|sts|stt|stx|ldch|stch|add|sub|comp|j|jeq|jlt|jgt|tix)\\s+((@|#)\\s*)?((\\b([a-z]){1}\\w{0,7})|\\d{1,4}|\\*)\\s*((\\+|-)\\s*\\d{1,4})?\\s*(,\\s*x)?\\s*(;.*)?"; 
 		regex re(reg);
  		if(regex_match(this->line,re)){
-			//cout<<"Valid format 4"<<endl;
+			this->labeled = true;
+			this->formattype = 4;
 			return 1;		
 			}
  		else
@@ -141,6 +138,8 @@ int Statement::check_format4_unlab(){
 	 string reg = "\\s*\\+\\b(lda|ldb|ldf|ldl|lds|ldt|ldx|sta|stb|stf|stl|sts|stt|stx|ldch|stch|add|sub|comp|j|jeq|jlt|jgt|tix)\\s+((@|#)\\s*)?((\\b([a-z]){1}\\w{0,7})|\\d{1,4}|\\*)\\s*((\\+|-)\\s*\\d{1,4})?\\s*(,\\s*x)?\\s*(;.*)?"; 
 	 regex re(reg);
  		if(regex_match(this->line,re)){
+			this->labeled = false;
+			this->formattype = 4;
 			//cout<<"Valid format 4"<<endl;
 			return 1;		
 			}
@@ -159,7 +158,8 @@ int Statement::check_format4_unlab(){
 	 string reg = "\\s*((\\b([a-z]){1}\\w{0,7})\\s+)\\b(lda|ldb|ldf|ldl|lds|ldt|ldx|sta|stb|stf|stl|sts|stt|stx|ldch|stch|add|sub|comp|j|jeq|jlt|jgt|tix)\\s+((@|#)\\s*)?((\\b([a-z]){1}\\w{0,7})|\\d{1,4}|\\*)\\s*((\\+|-)\\s*\\d{1,4})?\\s*(,\\s*x)?\\s*(;.*)?"; 
 	 regex re(reg);
  		if(regex_match(this->line,re)){
-			//cout<<"Valid format 3"<<endl;
+			this->labeled = true;
+			this->formattype = 3;
 			return 1;		
 			}
  		else
@@ -176,7 +176,8 @@ int Statement::check_format3_unlab(){
 	 string reg = "\\s*\\b(lda|ldb|ldf|ldl|lds|ldt|ldx|sta|stb|stf|stl|sts|stt|stx|ldch|stch|add|sub|comp|j|jeq|jlt|jgt|tix)\\s+((@|#)\\s*)?((\\b([a-z]){1}\\w{0,7})|\\d{1,4}|\\*)\\s*((\\+|-)\\s*\\d{1,4})?\\s*(,\\s*x)?\\s*(;.*)?"; 
 	 regex re(reg);
  		if(regex_match(this->line,re)){
-			//cout<<"Valid format 3"<<endl;
+			this->labeled = false;
+			this->formattype = 3;
 			return 1;		
 			}
  		else
@@ -194,7 +195,8 @@ int Statement::check_format2_lab(){
 	 string reg = "\\s*((\\b([a-z]){1}\\w{0,7})\\s+)((\\b(rmo|addr|subr|compr)\\s+\\b(a|x|l|b|s|t|f)\\s*,\\s*)|(\\b(tixr)\\s+))\\b(a|x|l|b|s|t|f)\\s*(;.*)?"; 
 	 regex re(reg);
  		if(regex_match(this->line,re)){
-			//cout<<"Valid format 2"<<endl;
+			this->labeled = true;
+			this->formattype = 2;
 			return 1;		
 			}
  		else
@@ -212,7 +214,8 @@ int Statement::check_format2_unlab(){
 	 string reg = "\\s*((\\b(rmo|addr|subr|compr)\\s+\\b(a|x|l|b|s|t|f)\\s*,\\s*)|(\\b(tixr)\\s+))\\b(a|x|l|b|s|t|f)\\s*(;.*)?"; 
 	 regex re(reg);
  		if(regex_match(this->line,re)){
-			//cout<<"Valid format 2"<<endl;
+			this->labeled = false;
+			this->formattype = 2;
 			return 1;		
 			}
  		else
@@ -225,11 +228,12 @@ int Statement::check_format2_unlab(){
 		
 	 
 int Statement::check_dir_lab(){
-		string input;
+	 string input;
 	 string reg = "\\s*(\\b([a-z]){1}\\w{0,7})\\s+((\\b(start)\\s+[a-f0-9]{1,4})|(\\b(byte)\\s+((c'.{1,14}')|(x'[a-f0-9]{1,15}')))|(\\b(word)\\s+-?\\d{1,4}(,-?\\d{1,4})*)|(\\b(resw|resb)\\s+\\d{1,4})|(\\b(equ)\\s+\\b([a-z]){1}\\w{1,7}(\\+\\d{1,4})?))\\s*(;.*)?";
 	 regex re(reg);
  		if(regex_match(this->line,re)){
-			//cout<<"Valid"<<endl;
+			this->labeled = true;
+			this->formattype = 1; 
 			return 1;		
 			}
  		else
@@ -247,12 +251,13 @@ int Statement::check_dir_unlab(){
 	 regex re(reg);
 		
  		if(regex_match(this->line,re)){
-			//cout<<"Valid"<<endl;
+			this->labeled = false;
+			this->formattype = 2;
 			return 1;		
 			}
  		else
  		{
- 			//cout<<"Invalid input"<<endl;
+ 		
  			return 0;
  		} 
 		 
@@ -262,19 +267,18 @@ int Statement::check_dir_unlab(){
 	 //
 	 void Statement::divide_operands(){
 		 
-		 
+		 string matched;
+		operand2 = extract(this->operand,"^,\\w");
 		 
 		 
 		 }
 	 
 	 
-	 //void partition_dir(bool label , string exp , statement &st );
-	 void Statement::partition_dir(bool label){
+	 void Statement::partition_dir(){
 		 string matched;
-	regex reg("^\\s*[\\w]+\\s*");
+		 regex reg("^\\s*[\\w]+\\s*");
 	
-	//I think this makes them point to the same value we need clone
-	string exp = this->line;
+		string exp = this->line;
 	
 	//extracting comment
 	matched = extract(exp,";.*");
@@ -283,25 +287,27 @@ int Statement::check_dir_unlab(){
 			
 	//trimming :
 	ltrim(exp);
-	//extract(exp,"^\\s*");
-	//matched.erase();
 	
-	if(label){
+	if(this->labeled){
 	//target : label - operation - operand - comment
 			
 			matched = extract(exp,"^\\s*[\\w]+\\s*"); //label
-			rtrim(matched);
+			trim_(matched);
+			
+			//rtrim(matched);
 			this->label = matched;
 			matched.erase();
 		}	
 			ltrim(exp);
 			matched = extract(exp,"^\\s*[\\w]+\\s*"); //operation
-		    rtrim(matched);
+		    //rtrim(matched);
+		    trim_(matched);
 		    this->operation = matched;
 		    
 		    matched.erase(); 
 		    rtrim(exp);
 		    ltrim(exp);
+		    trim_(exp);
 		    this->operand = exp;
 		 
 		 }
@@ -310,31 +316,23 @@ int Statement::check_dir_unlab(){
 	 
 	 
 	 void Statement::get_partitioned(){
-		 bool label = true;
-		
+		 		
 		string exp = this->line;
 		
-		int format = check_statment();
-		if(format%2 == 0) label = false;
-		this->formattype = format;
-		switch(format){
+		switch(this->formattype){
 			
 			case 0:
 			this->comment = exp;
 			break;
-			case 10:
-			case 11:
-			case 20:
-			case 21:
-			case 30:
-			case 31:
 			
-			partition_dir(label);
-			
+			case 1:
+			case 2:
+			case 3:			
+			partition_dir();
 			break;
-			case 40:
-			case 41:
-			partition_format4(label);
+			
+			case 4:
+			partition_format4();
 			break;
 			
 			default:
@@ -349,9 +347,10 @@ int Statement::check_dir_unlab(){
 		 }
 	 
 	 //void partition_format4(bool label , string exp , statement &st);
-	 void Statement::partition_format4(bool label){
+	 void Statement::partition_format4(){
 		 string matched;
 		 string exp = this->line;
+		 
 	//extracting comment
 	matched = extract(exp,";.*");
 	this->comment = matched;
@@ -360,7 +359,7 @@ int Statement::check_dir_unlab(){
 	//trimming :
 	ltrim(exp);
 		
-	if(label){
+	if(this->labeled){
 	//target : label - operation - operand - comment
 			
 			matched = extract(exp,"^\\s*[\\w]+\\s*"); //label
@@ -392,8 +391,8 @@ int Statement::check_dir_unlab(){
 	
 		 }
 		 
-		 
-	 void Statement::trim(string &str){
+		  
+	  void Statement::trim(string &str){
 		 
 		 if(str.size()){
 			regex ret(".[^\\s]+");
@@ -402,13 +401,14 @@ int Statement::check_dir_unlab(){
 
 		 
 		 }
+
 	
 	 string Statement::extract(string &exp , string re){
 		 
 		 regex reg(re);
 		string matched;
 		get_matched(exp, reg , matched);
-		//trim(matched);
+		trim(matched);
 		exp = regex_replace(exp,reg, "");
 		//cout<<"<"<<exp<<"<"<<endl;
 		return matched;
@@ -418,35 +418,46 @@ int Statement::check_dir_unlab(){
 		 regex reg("^\\s*");
 		string mat;
 		get_matched(exp , reg,mat);
-		exp = exp = regex_replace(exp,reg,"");
+		exp = regex_replace(exp,reg,"");
 	 }
 	 
 	 
 	 void Statement::rtrim(string &exp){
-		 regex reg("(\\s*)$");
+	regex reg("(\\s*)$");
 	string mat;
-	get_matched(exp , reg,mat);
-	exp = exp = regex_replace(exp,reg,"");
+	get_matched(exp ,reg,mat);
+	exp = regex_replace(exp,reg,"");
 		 
 		 }
 
-
-
+	void Statement::trim_(string &exp , string delim){
+	regex reg("\\s+");
+	exp = regex_replace(exp,reg,delim);
+	}
+		
+//void trim_(string &s , string delim="");
 
 int main(){
 	Statement st;
-	//st.check_statment("start    1000   ;commenty");
-	st.line = "start	1000	;ddd";
+	//st.check_statement("start    1000   ;commenty");
+	st.line = "		rmo		a   ,	x ;fff";
+	//st.trim(st.line);
+	st.check_statement();
 	st.get_partitioned();
 	
 	cout<<"*.*.*.*.*.*.*.*.***...**...**..**"<<endl;
 			cout<<"<"<<st.label<<">"<<endl;
 			cout<<"<"<<st.operation<<">"<<endl;
 			cout<<"<"<<st.operand<<">"<<endl;
+			cout<<"<"<<st.operand2<<">"<<endl;
 			cout<<"<"<<st.comment<<">"<<endl;
+			cout<<"<"<<st.line<<">"<<endl;
 	cout<<"*.*.*.*.*.*.*.*.***...**...**..**"<<endl;
 	
-	
+	//string ss("Hello my beauty ");
+	//trim_(ss);
+	//cout<<ss;
+	cout<<sizeof(st);
 	return 0;
 	
 	}
